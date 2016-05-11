792a1d464c9ed200426496b23817f80bf932d4c1
Abhishek Verma <www.abhiverma23@gmail.com>
2016-05-11 15:08:05 +0530

    Make layout better

--- java/com/etechclub/kuchbhi/ControlPanel.java
+++ java/com/etechclub/kuchbhi/ControlPanel.java
@@ -1,12 +1,18 @@
 package com.etechclub.kuchbhi;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ControlPanel extends AppCompatActivity {
 
+    private boolean toggleSwitch1 = false;
+    private boolean toggleSwitch2 = false;
+    private boolean toggleSwitch3 = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +26,23 @@ public class ControlPanel extends AppCompatActivity {
 
         return true;
     }
+
+    public void connect(MenuItem item){
+        TextView textView = (TextView) findViewById(R.id.connect_status);
+        textView.setTextColor(Color.WHITE);
+        textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        textView.setText("Connected");
+    }
+
+    /*public void toggleSwitch1(View view) {
+        TextView textView = (TextView)findViewById(R.id.toggle_text_switch1);
+        ImageView imageView = (ImageView)findViewById(R.id.toggle_button_switch1);
+        toggleSwitch1 = toggleSwitch1 ? false : true;
+        if(toggleSwitch1){
+            textView.setText("ON");
+            imageView.setImageResource(R.string.);
+        }else{
+            textView.setText("OFF");
+        }
+    }*/
 }
