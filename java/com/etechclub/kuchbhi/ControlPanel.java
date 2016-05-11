27c6ca9779f2a4fa0ee9e0e0711b84f653d8aee4
Abhishek Verma <www.abhiverma23@gmail.com>
2016-05-11 21:40:15 +0530

    Add support to http

--- java/com/etechclub/kuchbhi/ControlPanel.java
+++ java/com/etechclub/kuchbhi/ControlPanel.java
@@ -1,7 +1,9 @@
 package com.etechclub.kuchbhi;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -12,12 +14,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
+
 public class ControlPanel extends AppCompatActivity {
 
     boolean toggleSwitch1;
     boolean toggleSwitch2;
     boolean toggleSwitch3;
     boolean isConnected;
+
+    String ip = "192.168.192.1";
+    final String ping = "ping/ping.php", task = "task.php";
+    int port = 8080;
+
+    InputStream is = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,6 +138,11 @@ public class ControlPanel extends AppCompatActivity {
         toggleSwitch2 = bundle.getBoolean("switch2");
         toggleSwitch3 = bundle.getBoolean("switch3");
         isConnected = bundle.getBoolean("isConnected");
+        if (bundle.containsKey("ip")) {
+            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No", Toast.LENGTH_SHORT).show();
+        }
 
         TextView textView = (TextView) findViewById(R.id.toggle_text_switch1);
         ImageView imageView = (ImageView) findViewById(R.id.toggle_button_switch1);
@@ -158,8 +179,41 @@ public class ControlPanel extends AppCompatActivity {
 
     }
 
+    public void editConnection(MenuItem item) {
+        Intent intent = new Intent("com.etechclub.kuchbhi.EDITCONNECTION");
+        startActivity(intent);
+    }
 
-/**
- * Code from here belongs to connecting to board
- */
+
+    /**
+     * Code from here belongs to connecting to board
+     */
+    boolean turn(String s, String tdo) {
+        boolean check = true;
+        try {
+            /*Log.e("Pass 1.0.1", "Going in big nap");
+            Thread.sleep(20000);
+            Log.e("Pass 1.0.1", "Aaah that was a real big nap....");*/
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            Log.e("Pass 1.0", "Line 1 Executed");
+            StrictMode.setThreadPolicy(policy);
+            Log.e("Pass 1.0", "Line 2 Executed");
+            @SuppressWarnings("deprecation") HttpClient httpclient = new DefaultHttpClient();
+            Log.e("Pass 1.0", "httpclient created");
+            @SuppressWarnings("deprecation") HttpPost httppost = new HttpPost(
+                    "http://" + ip + ":" + port + "/php_project/" + tdo +"/?" + s);
+            Log.e("Pass 1.0", "Link setup done");
+            HttpResponse response = httpclient.execute(httppost);
+            Log.e("Pass 1.0", "Link executed");
+            HttpEntity entity = response.getEntity();
+            Log.e("Pass 1.0", "Entity setup success");
+            is = entity.getContent();
+            check = true;
+        } catch (Exception e) {
+            Log.e("Fail 1", e.toString());
+            check = false;
+        } finally {
+            return check;
+        }
+    }
 }
